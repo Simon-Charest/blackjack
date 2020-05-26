02d14fbf7db10e58951cf45e9c272fd684dc8326
Simon Charest <simoncharest@gmail.com>
2020-05-26 13:52:09 -0400

    Adding cards

--- blackjack/common/constant/constant.py
+++ blackjack/common/constant/constant.py
@@ -17,14 +17,445 @@ ICON = os.path.join(ROOT_DIR, 'resources/blackjack-icon.png')
 
 """
 Deck:
-    Values:
+    Ranks:
         King
         Queen
         Jack (Knave)
-    Kinds:
+    Suits:
         Club (Hound Tethers)
         Heart (Hunting Horns)
         Spade (Game Nooses)
         Diamond (Dog Collars)
-Source: The Cloisters Playing Cards (https://www.metmuseum.org/art/collection/search/475513)
+    Colors:
+        Club: green
+        Diamond: blue
+        Heart: Red
+        Spade: black
+Sources:
+    Four-color deck (https://en.wikipedia.org/wiki/Four-color_deck)
+    Standard 52-card deck (https://en.wikipedia.org/wiki/Standard_52-card_deck)
+    The Cloisters Playing Cards (https://www.metmuseum.org/art/collection/search/475513)
 """
+
+DECKS = {
+    'cloisters': {
+        '♣A': {
+            'value': [1, 11],
+            'filename': 'c01.png'
+        },
+        '♣2': {
+            'value': 2,
+            'filename': 'c02.png'
+        },
+        '♣3': {
+            'value': 3,
+            'filename': 'c03.png'
+        },
+        '♣4': {
+            'value': 4,
+            'filename': 'c04.png'
+        },
+        '♣5': {
+            'value': 5,
+            'filename': 'c05.png'
+        },
+        '♣6': {
+            'value': 6,
+            'filename': 'c06.png'
+        },
+        '♣7': {
+            'value': 7,
+            'filename': 'c07.png'
+        },
+        '♣8': {
+            'value': 8,
+            'filename': 'c08.png'
+        },
+        '♣9': {
+            'value': 9,
+            'filename': 'c09.png'
+        },
+        '♣10': {
+            'value': 10,
+            'filename': 'c10.png'
+        },
+        '♣J': {
+            'value': 10,
+            'filename': 'c11.png'
+        },
+        '♣Q': {
+            'value': 10,
+            'filename': 'c12.png'
+        },
+        '♣K': {
+            'value': 10,
+            'filename': 'c13.png'
+        },
+        '◆A': {
+            'value': [1, 11],
+            'filename': 'd01.png'
+        },
+        '◆2': {
+            'value': 2,
+            'filename': 'd02.png'
+        },
+        '◆3': {
+            'value': 3,
+            'filename': 'd03.png'
+        },
+        '◆4': {
+            'value': 4,
+            'filename': 'd04.png'
+        },
+        '◆5': {
+            'value': 5,
+            'filename': 'd05.png'
+        },
+        '◆6': {
+            'value': 6,
+            'filename': 'd06.png'
+        },
+        '◆7': {
+            'value': 7,
+            'filename': 'd07.png'
+        },
+        '◆8': {
+            'value': 8,
+            'filename': 'd08.png'
+        },
+        '◆9': {
+            'value': 9,
+            'filename': 'd09.png'
+        },
+        '◆10': {
+            'value': 10,
+            'filename': 'd10.png'
+        },
+        '◆J': {
+            'value': 10,
+            'filename': 'd11.png'
+        },
+        '◆Q': {
+            'value': 10,
+            'filename': 'd12.png'
+        },
+        '◆K': {
+            'value': 10,
+            'filename': 'd13.png'
+        },
+        '♥A': {
+            'value': [1, 11],
+            'filename': 'h01.png'
+        },
+        '♥2': {
+            'value': 2,
+            'filename': 'd02.png'
+        },
+        '♥3': {
+            'value': 3,
+            'filename': 'd03.png'
+        },
+        '♥4': {
+            'value': 4,
+            'filename': 'd04.png'
+        },
+        '♥5': {
+            'value': 5,
+            'filename': 'd05.png'
+        },
+        '♥6': {
+            'value': 6,
+            'filename': 'd06.png'
+        },
+        '♥7': {
+            'value': 7,
+            'filename': 'd07.png'
+        },
+        '♥8': {
+            'value': 8,
+            'filename': 'd08.png'
+        },
+        '♥9': {
+            'value': 9,
+            'filename': 'd09.png'
+        },
+        '♥10': {
+            'value': 10,
+            'filename': 'd10.png'
+        },
+        '♥J': {
+            'value': 10,
+            'filename': 'd11.png'
+        },
+        '♥Q': {
+            'value': 10,
+            'filename': 'd12.png'
+        },
+        '♥K': {
+            'value': 10,
+            'filename': 'd13.png'
+        },
+        '♠A': {
+            'value': [1, 11],
+            'filename': 's01.png'
+        },
+        '♠2': {
+            'value': 2,
+            'filename': 'd02.png'
+        },
+        '♠3': {
+            'value': 3,
+            'filename': 'd03.png'
+        },
+        '♠4': {
+            'value': 4,
+            'filename': 'd04.png'
+        },
+        '♠5': {
+            'value': 5,
+            'filename': 'd05.png'
+        },
+        '♠6': {
+            'value': 6,
+            'filename': 'd06.png'
+        },
+        '♠7': {
+            'value': 7,
+            'filename': 'd07.png'
+        },
+        '♠8': {
+            'value': 8,
+            'filename': 'd08.png'
+        },
+        '♠9': {
+            'value': 9,
+            'filename': 'd09.png'
+        },
+        '♠10': {
+            'value': 10,
+            'filename': 'd10.png'
+        },
+        '♠J': {
+            'value': 10,
+            'filename': 'd11.png'
+        },
+        '♠Q': {
+            'value': 10,
+            'filename': 'd12.png'
+        },
+        '♠K': {
+            'value': 10,
+            'filename': 'd13.png'
+        }
+    },
+    'standard': {
+        '♣A': {
+            'value': [1, 11],
+            'filename': 'c01.svg'
+        },
+        '♣2': {
+            'value': 2,
+            'filename': 'c02.svg'
+        },
+        '♣3': {
+            'value': 3,
+            'filename': 'c03.svg'
+        },
+        '♣4': {
+            'value': 4,
+            'filename': 'c04.svg'
+        },
+        '♣5': {
+            'value': 5,
+            'filename': 'c05.svg'
+        },
+        '♣6': {
+            'value': 6,
+            'filename': 'c06.svg'
+        },
+        '♣7': {
+            'value': 7,
+            'filename': 'c07.svg'
+        },
+        '♣8': {
+            'value': 8,
+            'filename': 'c08.svg'
+        },
+        '♣9': {
+            'value': 9,
+            'filename': 'c09.svg'
+        },
+        '♣10': {
+            'value': 10,
+            'filename': 'c10.svg'
+        },
+        '♣J': {
+            'value': 10,
+            'filename': 'c11.svg'
+        },
+        '♣Q': {
+            'value': 10,
+            'filename': 'c12.svg'
+        },
+        '♣K': {
+            'value': 10,
+            'filename': 'c13.svg'
+        },
+        '◆A': {
+            'value': [1, 11],
+            'filename': 'd01.svg'
+        },
+        '◆2': {
+            'value': 2,
+            'filename': 'd02.svg'
+        },
+        '◆3': {
+            'value': 3,
+            'filename': 'd03.svg'
+        },
+        '◆4': {
+            'value': 4,
+            'filename': 'd04.svg'
+        },
+        '◆5': {
+            'value': 5,
+            'filename': 'd05.svg'
+        },
+        '◆6': {
+            'value': 6,
+            'filename': 'd06.svg'
+        },
+        '◆7': {
+            'value': 7,
+            'filename': 'd07.svg'
+        },
+        '◆8': {
+            'value': 8,
+            'filename': 'd08.svg'
+        },
+        '◆9': {
+            'value': 9,
+            'filename': 'd09.svg'
+        },
+        '◆10': {
+            'value': 10,
+            'filename': 'd10.svg'
+        },
+        '◆J': {
+            'value': 10,
+            'filename': 'd11.svg'
+        },
+        '◆Q': {
+            'value': 10,
+            'filename': 'd12.svg'
+        },
+        '◆K': {
+            'value': 10,
+            'filename': 'd13.svg'
+        },
+        '♥A': {
+            'value': [1, 11],
+            'filename': 'h01.svg'
+        },
+        '♥2': {
+            'value': 2,
+            'filename': 'd02.svg'
+        },
+        '♥3': {
+            'value': 3,
+            'filename': 'd03.svg'
+        },
+        '♥4': {
+            'value': 4,
+            'filename': 'd04.svg'
+        },
+        '♥5': {
+            'value': 5,
+            'filename': 'd05.svg'
+        },
+        '♥6': {
+            'value': 6,
+            'filename': 'd06.svg'
+        },
+        '♥7': {
+            'value': 7,
+            'filename': 'd07.svg'
+        },
+        '♥8': {
+            'value': 8,
+            'filename': 'd08.svg'
+        },
+        '♥9': {
+            'value': 9,
+            'filename': 'd09.svg'
+        },
+        '♥10': {
+            'value': 10,
+            'filename': 'd10.svg'
+        },
+        '♥J': {
+            'value': 10,
+            'filename': 'd11.svg'
+        },
+        '♥Q': {
+            'value': 10,
+            'filename': 'd12.svg'
+        },
+        '♥K': {
+            'value': 10,
+            'filename': 'd13.svg'
+        },
+        '♠A': {
+            'value': [1, 11],
+            'filename': 's01.svg'
+        },
+        '♠2': {
+            'value': 2,
+            'filename': 'd02.svg'
+        },
+        '♠3': {
+            'value': 3,
+            'filename': 'd03.svg'
+        },
+        '♠4': {
+            'value': 4,
+            'filename': 'd04.svg'
+        },
+        '♠5': {
+            'value': 5,
+            'filename': 'd05.svg'
+        },
+        '♠6': {
+            'value': 6,
+            'filename': 'd06.svg'
+        },
+        '♠7': {
+            'value': 7,
+            'filename': 'd07.svg'
+        },
+        '♠8': {
+            'value': 8,
+            'filename': 'd08.svg'
+        },
+        '♠9': {
+            'value': 9,
+            'filename': 'd09.svg'
+        },
+        '♠10': {
+            'value': 10,
+            'filename': 'd10.svg'
+        },
+        '♠J': {
+            'value': 10,
+            'filename': 'd11.svg'
+        },
+        '♠Q': {
+            'value': 10,
+            'filename': 'd12.svg'
+        },
+        '♠K': {
+            'value': 10,
+            'filename': 'd13.svg'
+        }
+    }
+}
